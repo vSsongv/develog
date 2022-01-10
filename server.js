02b3bf0d8846644bac0f7d327045356298185230
JJongBin <gghkdu2@gmail.com>
2022-01-10 13:20:05 +0900

    feat: Init develog

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const db = require('./db');
+
+const app = express();
+const PORT = 9000;
+
+app.use(express.static('src'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
