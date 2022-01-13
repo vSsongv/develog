05a1dbea94dae2434a37f246e2b08d52737b9a30
Minsung Kim <minsoftk@gmail.com>
2022-01-13 13:28:42 +0900

    fix: fix dynamic-data's name. add event logic
    - add detailEvent function

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,6 +1,4 @@
-const $root = document.querySelector('.root');
-
-const detailHtml = `  <header class="header">
+const detailHTML = `<header class="header">
 	<h1 class="header--logo">develog</h1>
 
 	<form class="search--form" action="">
@@ -24,10 +22,10 @@ const detailHtml = `  <header class="header">
 </header>
 <main class="detail-container">
 	<section class="detail">
-		<h1 class="detail__title">포스트 제목 (동적데이터)</h1>
+		<h1 class="detail__title">postTitle</h1>
 		<div class="detail__info">
 			<img class="avatar-button avatar-button--size" src="./assets/종빈이.png" alt="avatar-button" />
-			<span class="author">작성자 이름(동적데이터)</span>
+			<span class="author">postAuthor</span>
 			<button class="no-display heart-btn btn">
 				<i class="far fa-heart"></i>
 			</button>
@@ -43,32 +41,7 @@ const detailHtml = `  <header class="header">
 		</div>
 
 		<pre class="detail__content">
-			<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum labore excepturi repudiandae earum nihil quis
-			</h1>numquam minus, natus corrupti molestiae blanditiis iure distinctio repellendus, praesentium tenetur! Nisi    tempore repudiandae commodi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae soluta doloremque
-			harum adipisci aliquid quam, explicabo cumque eius, veniam iure quas repudiandae quo voluptatibus blanditiis
-			at eligendi dignissimos? Non, repellat.
-			<br />
-			<br />
-			<br />
-			<br />
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum labore excepturi repudiandae earum nihil quis
-			numquam minus, natus corrupti molestiae blanditiis iure distinctio repellendus, praesentium tenetur! Nisi
-			tempore repudiandae commodi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae soluta doloremque
-			harum adipisci aliquid quam, explicabo cumque eius, veniam iure quas repudiandae quo voluptatibus blanditiis
-			at eligendi dignissimos? Non, repellat.
-			<br />
-			<br />
-			<br />
-			<br />
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum labore excepturi repudiandae earum nihil quis
-			numquam minus, natus corrupti molestiae blanditiis iure distinctio repellendus, praesentium tenetur! Nisi
-			tempore repudiandae commodi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae soluta doloremque
-			harum adipisci aliquid quam, explicabo cumque eius, veniam iure quas repudiandae quo voluptatibus blanditiis
-			at eligendi dignissimos? Non, repellat.
-			<br />
-			<br />
-			<br />
-			<br />
+			postContent
 		</pre>
 	</section>
 
@@ -77,7 +50,7 @@ const detailHtml = `  <header class="header">
 		<div class="comment__user-info">
 			<img
 				class="avatar-button avatar-button--size"
-				src="./assets/종빈이.png"
+				src=imgUrl
 				class="user__avatar"
 				alt="user-avatar"
 			/>
@@ -99,7 +72,7 @@ const detailHtml = `  <header class="header">
 			<div class="comment__user-info">
 				<img
 					class="avatar-button avatar-button--size"
-					src="./assets/종빈이.png"
+					src=imgUrl
 					class="user__avatar"
 					alt="user-avatar"
 				/>
@@ -116,4 +89,17 @@ const detailHtml = `  <header class="header">
 	</section>
 </main>`;
 
-export default { detailHtml };
+const detailEvent = () => {
+  document.querySelector('.header--logo').addEventListener('click', () => {
+    window.history.pushState({ data: 'index' }, '', '/');
+  });
+
+  document.querySelector();
+  const $userinfo = document.querySelector('.user-info');
+};
+
+// export default render;
+export default {
+  detailHTML,
+  detailEvent,
+};
