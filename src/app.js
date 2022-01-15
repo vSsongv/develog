363d5a8473bc0414a95e03caeec2456df8851a85
Minsung Kim <minsoftk@gmail.com>
2022-01-16 02:02:32 +0900

    feat: add detail page events
    - html : add button tag in html file, for add click events
    - js : using axios for fetchData(post), add events '.detail__info' parts
    - scss : add 'none' class for using display:none (heart-button)
    - scss : don't apply margin property for img display

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import index from './js/index';
+
+import detailUrlEvents from './js/detail';
+
 import signin from './js/signin';
 import signup from './js/signup';
-import detail from './js/detail';
 import mypage from './js/mypage';
 import mypageEdit from './js/mypageEdit';
 import header from './js/header';
+import writeUrlEvents from './js/write';
 
 const history = require('history-events');
 
@@ -17,14 +20,14 @@ const render = html => {
 const switchDependsOnUrl = () => {
   const url = window.location.pathname.split('/');
   const pageCase = url[1] === '' ? '/' : `/${url[1]}`;
+  console.log(pageCase);
   switch (pageCase) {
     case '/':
       render(index.indexHtml);
       index.indexEvent();
       break;
     case '/detail':
-      render(detail.detailHtml);
-      detail.detailEvent();
+      detailUrlEvents();
       break;
     case '/signin':
       render(signin.signinHtml);
@@ -45,12 +48,13 @@ const switchDependsOnUrl = () => {
       render(mypageEdit.mypageEditHtml);
       mypageEdit.mypageEditEvent();
       break;
-    case 'upload':
-      // upload
+    case '/write':
+      writeUrlEvents();
       break;
     default:
       // 404page 뿌려주기
       alert('잘못된 페이지입니다.');
+      window.history.pushState('404 error', '', '/');
   }
 };
 
