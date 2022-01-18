7b2959877bbf00c293bf2ae9045ae8847faeb947
ktmihs <yellowgreen423@gmail.com>
2022-01-19 03:01:45 +0900

    refactor: refactoring index, app

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -3,7 +3,7 @@ import Main from './main';
 
 // import detailUrlEvents from './js/detail';
 import Signin from './signin';
-// import signup from './js/signup';
+import Signup from './signup';
 // import develog from './js/develog';
 // import mypage from './js/mypage';
 // import mypageEdit from './js/mypageEdit';
@@ -13,12 +13,21 @@ import Signin from './signin';
 
 const routes = [
   // { path: '/', component: render(mainNode) },
-  { path: '/', component: Main },
+  {
+    path: '/',
+    component: Main
+  },
   // { path: '/search', component: Search },
   // { path: '/develog', component: Develog },
   // { path: '/detail', component: Detail },
-  { path: '/signin', component: Signin },
-  // { path: '/signup', component: Signup },
+  {
+    path: '/signin',
+    component: Signin
+  },
+  {
+    path: '/signup',
+    component: Signup
+  },
   // { path: '/mypage', component: Mypage },
   // { path: '/mypageEdit', component: MypageEdit },
   // { path: '/write', component: Write },
@@ -29,7 +38,7 @@ const router = () => {
   const path = window.location.pathname.split('/');
   const targetPath = '/' + path[1];
 
-  return [, ...routes.find(route => route.path === targetPath).component()];
+  return [...routes.find(route => route.path === targetPath).component()];
 };
 
 export default router;
