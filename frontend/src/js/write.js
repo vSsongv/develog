d4a123a1a823274e0a6bc69abfb2a5faa6fa7882
JJongBin <gghkdu2@gmail.com>
2022-01-19 20:44:54 +0900

    feat: render write page

--- frontend/src/js/write.js
+++ frontend/src/js/write.js
@@ -1,27 +1,15 @@
 import write from '../html/write.html';
 
-const Editor = require('@toast-ui/editor');
-
-// const editor = new Editor({
-//   el: document.querySelector('#editor'),
-//   height: '500px',
-//   initialEditType: 'markdown',
-//   previewStyle: 'vertical',
-// });
-
 const $root = document.querySelector('.root');
 
-const writeRender = _html => {
-  $root.innerHTML = _html;
-};
+const writeNode = () => {
+  const node = document.createElement('div');
+  node.innerHTML = write;
 
-const writeUrlEvents = () => {
-  // editor.getHTML();
-  writeRender(write);
-  document.querySelector('.upload__form__submit').addEventListener('click', e => {
+  node.querySelector('.upload__form__submit').addEventListener('click', e => {
     e.preventDefault();
     console.log('test');
   });
+  return node.children;
 };
-
-export default writeUrlEvents;
+export default writeNode;
