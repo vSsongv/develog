692ed10971f05187bb790d7b4c5cd715614e0adb
JJongBin <gghkdu2@gmail.com>
2022-01-19 14:40:15 +0900

    fix: separate js, html - (mypage, mypageEdit)

--- frontend/src/js/mypage.js
+++ frontend/src/js/mypage.js
@@ -1,65 +1,9 @@
 import axios from 'axios';
+import mypage from '../html/mypage.html';
 
 const mypageNode = () => {
   const node = document.createElement('div');
-  node.innerHTML = `
-<div class="cover hidden"></div>
-
-<div class="container">
-  <section class="user-profile">
-    <h2 class="a11yHidden">회원 정보</h2>
-    <div class="user-profile-avatar"></div>
-    <div class="user-profile-info">
-      <div class="nickname">
-        <span></span>
-        <span>님, 환영합니다.</span>
-      </div>
-      <div class="user-profile-info-input">
-        <label for="name">name :</label>
-        <input id="name" type="text" disabled>
-      </div>
-      <div class="user-profile-info-input">
-        <label for="email">email :</label>
-        <input id="email" type="email" disabled>
-      </div>
-      <div class="user-profile-info-input">
-        <label for="phone">phone :</label>
-        <input id="phone" type="text" disabled>
-      </div>
-    </div>
-  </section>
-
-  <section class="information">
-    <h2 class="a11yHidden">좋아요한 블로그 포스팅 목록</h2>
-    <div class="postNum">
-      <span>내 블로그 글 개수</span>
-      <span>12</span>
-    </div>
-
-  </section>
-  <section class="button-box">
-    <button class="button button--edit">회원정보 수정</button>
-    <button class="button button--withdrawal">회원탈퇴</button>
-  </section>
-
-  <section class="withdrawal hidden">
-    <h2 class="a11yHidden">회원탈퇴 확인 팝업</h2>
-    <button class="withdrawal--close"><i class="fas fa-times"></i></button>
-    <h3>회원탈퇴를 하시려면 비밀번호를 입력후 회원탈퇴 버튼을 눌러주세요.</h3>
-    <input class="withdrawal--password" type="password" placeholder="비밀번호를 입력해주세요.">
-    <span class="error-message hidden">비밀번호가 일치하지 않습니다. 3회 이상 틀리면 로그아웃됩니다.</span>
-    <button type="button" class="button button--withdrawal withdrawal-confirm">회원탈퇴</button>
-  </section>
-  
-  <section class="profileEdit hidden">
-    <h2 class="a11yHidden">수정페이지 이동 전 비밀번호 확인</h2>
-    <button class="profileEdit--close"><i class="fas fa-times"></i></button>
-    <h3>수정페이지로 이동하시려면 비밀번호를 입력하세요.</h3>
-    <input class="profileEdit--password" type="password" placeholder="비밀번호를 입력해주세요.">
-    <span class="error-message hidden">비밀번호가 일치하지 않습니다. 3회 이상 틀리면 로그아웃됩니다.</span>
-    <button type="button" class="button button--edit profileEdit-confirm">비밀번호 확인</button>
-  </section>
-</div>`;
+  node.innerHTML = mypage;
 
   // Event
   (async () => {
