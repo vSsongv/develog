218a734bdc0a34260a4ee0b17a7038faf3cbca12
vSsongv <songdh21@naver.com>
2022-01-21 13:22:55 +0900

    feat: not found layout done

--- frontend/src/js/notFound.js
+++ frontend/src/js/notFound.js
@@ -1,10 +1,10 @@
 import notFound from '../html/notFound.html';
 
 const notFoundNode = () => {
-  const node = document.createElement('div');
+  const node = document.getElementById('root');
   node.innerHTML = notFound;
 
   return node.children;
-}
+};
 
 export default notFoundNode;
